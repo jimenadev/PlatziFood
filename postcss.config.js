441ea86dbbe1b0426bffc9c4b57e6c50fdf1420f
Jimena <jimena@Mac-mini-de-Jimena.local>
2024-01-06 13:11:01 -0300

    add purge and nano css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        purgecss({
+          content: ['./**/*.html']
+        }),
+        require('cssnano')({
+          preset: 'default',
+      }),
       ]
 }
