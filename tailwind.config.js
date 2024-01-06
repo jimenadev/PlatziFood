b1efa9cd75fd675e765e74df1da9782cd40f12b8
Jimena <jimena@Mac-mini-de-Jimena.local>
2024-01-06 01:08:25 -0300

    add banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         'primary':'#f3c614',
         'secondary':'#353535'
       },
+      height:{
+        '80':'20rem',
+        '100':'25rem'
+      }
     },
     accentColor: ({ theme }) => ({
       ...theme('colors'),
